462ebb4125697073ea3ace4aa653f2d86344be0f
Kumari Priya <priya@goel.pw>
2017-11-01 20:54:55 -0700

    add process.argv[] to get input

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 const mortgage = require('./morgage.js');
+let mPayment = parseInt(process.argv[2]);
+let nYears = parseInt(process.argv[3]);
+let iRate = parseInt(process.argv[4]);
 let {monthlyPayment, monthlyRate, amortization} =
-    mortgage.calculateAmortization(1000,5,9);
+mortgage.calculateAmortization(mPayment,nYears,iRate);
 console.log(monthlyPayment);
 console.log(monthlyRate);
 console.log(amortization);
